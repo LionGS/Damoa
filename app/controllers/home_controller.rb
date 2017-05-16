6899f4a2665b0d5e31404bcd490af07a6b20e14b
LionGS <gb1541@likelion.org>
2017-05-16 10:44:01 +0900

    popularity

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @posts = Totalpost.all.sample(20)
+    @posts = Totalpost.where("popurarity >= 3").sample(20)
     @post_no = 1
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170515092631) do
+ActiveRecord::Schema.define(version: 20170515135638) do
 
   create_table "totalpost", primary_key: "post_no", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.text    "source",      limit: 65535
@@ -36,6 +36,23 @@ ActiveRecord::Schema.define(version: 20170515092631) do
     t.text    "last_update",    limit: 65535
     t.integer "popurarity"
     t.text    "posttext",       limit: 65535
+  end
+
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
 end
