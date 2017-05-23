0c6956b540f9342f33b5a663ec34dbc056c9acbe
LionGS <gb1541@likelion.org>
2017-05-23 14:42:26 +0900

    tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  # protect_from_forgery with: :exception
+  before_action :configure_account_update_params, only: [:update]
+  protected
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:tag_list])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  acts_as_taggable
+  acts_as_taggable_on :skills, :interests
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
