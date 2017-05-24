dd240140a7f08576c85d31d5c78cdfb71bbbcac0
LionGS <gb1541@kangwon.ac.kr>
2017-05-24 09:25:15 +0900

    edit some ui

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @posts = Totalpost.where("popurarity >= 3").sample(20)
+    @posts = Totalpost.order("popurarity DESC").limit(20)
     @post_no = 1
   end
 end
