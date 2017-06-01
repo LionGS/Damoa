a9252238d69edc4a16e2cb68893cb05b9094ce5d
LionGS <gb1541@kangwon.ac.kr>
2017-06-01 17:39:18 +0900

    fix when sign in username didn't save problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   before_action :configure_account_update_params, only: [:update]
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_account_update_params
     devise_parameter_sanitizer.permit(:account_update, keys: [:tag_list])
+  end
+
+
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
 
 end
