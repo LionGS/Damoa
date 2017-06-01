4655cae4419332552b57f6d0ad1f06901fb1ef6c
LionGS <gb1541@kangwon.ac.kr>
2017-06-01 17:21:28 +0900

    demo

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,22 @@
 class RegistrationsController < Devise::RegistrationsController
-  private
+  def update
+    account_update_params = devise_parameter_sanitizer.sanitize(:account_update)
+    @user = User.find(current_user.id)
+    if needs_password?
+      successfully_updated = @user.update_with_password(account_update_params)
+    else
+      account_update_params.delete('password')
+      account_update_params.delete('password_confirmation')
+      account_update_params.delete('current_password')
+      successfully_updated = @user.update_attributes(account_update_params)
+    end
+    render 'edit'
 
-  def sign_up_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
 
-  def account_update_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation, :current_password)
+  def needs_password?
+    params[:user][:password].present?
   end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  attr_accessor :login
+  attr_accessor :current_password
 end
