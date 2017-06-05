405239ccfaca0bd6a225a3440c1b841c7a83e0ca
LeeGeunSik <gb1541@kangwon.ac.kr>
2017-06-05 21:14:33 +0900

    유저가 태그를 입력하면 태그에 맞는 게시물들을 선별해서 찾아주는것을 구현

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,23 @@
 class HomeController < ApplicationController
   def index
-    @posts = Totalpost.order("popurarity DESC").limit(20)
+    # @posts = Totalpost.order("popurarity DESC").limit(20)
+
+    if user_signed_in?
+      @posts = Array.new
+      current_user.tag_list.each do |tag|
+        Totalpost.where(["title LIKE ?", "%#{tag}%"]).each do |post|
+          @posts.push(post)
+        end
+        Totalpost.where(["posttext LIKE ?", "%#{tag}%"]).each do |post|
+          @posts.push(post)
+        end
+      end
+      @posts.sort_by! {|post| post.popurarity}.reverse!
+      @posts = @posts.first(20)
+    else
+      @posts = Totalpost.order("popurarity DESC").limit(20)
+    end
+
     @post_no = 1
   end
 end
