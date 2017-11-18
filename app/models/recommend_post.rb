56edc5c98caedd0754dcd6dd4a835f4474d0048f
LeeGeunSik <gb1541@kangwon.ac.kr>
2017-11-18 18:16:21 +0900

    pagination align center

--- app/models/recommend_post.rb
+++ app/models/recommend_post.rb
@@ -1,4 +1,5 @@
 class RecommendPost < ApplicationRecord
   belongs_to :totalpost
   belongs_to :user
+  paginates_per 20
 end
--- app/models/scraped_post.rb
+++ app/models/scraped_post.rb
@@ -1,4 +1,5 @@
 class ScrapedPost < ApplicationRecord
   belongs_to :user
   belongs_to :totalpost
+  paginates_per 50
 end
